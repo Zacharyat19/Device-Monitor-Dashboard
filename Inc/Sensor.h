bdeda558c1c026e5b0230d099508977ee7d7383b
Zacharyat19 <Zacharyat19@gmail.com>
2026-03-04 20:44:45 -0800

    Added the backend sensor.

---
+++ Inc/Sensor.h
@@ -0,0 +1,18 @@
+#include <QObject>
+#include <QTimer>
+
+class Sensor : public QObject 
+{
+    Q_OBJECT
+
+public:
+    Sensor(QObject *parent = nullptr);
+    void toggle();
+
+signals:
+    void dataReady(double value);
+
+private:
+    QTimer *timer;
+    void generate();
+};
---
+++ Src/Sensor.cpp
@@ -0,0 +1,27 @@
+#include "../Inc/Sensor.h"
+#include <QRandomGenerator>
+
+Sensor::Sensor(QObject *parent) : QObject(parent) 
+{
+    timer = new QTimer(this);
+    timer->setInterval(1000);
+    
+    connect(timer, &QTimer::timeout, this, &Sensor::generate);
+}
+
+void Sensor::toggle() 
+{
+    if (timer->isActive()) 
+    {
+        timer->stop();
+    } else 
+    {
+        timer->start();
+    }
+}
+
+void Sensor::generate() 
+{
+    double randomValue = QRandomGenerator::global()->bounded(20, 81);
+    emit dataReady(randomValue);
+}
